ba2419be8a42a5cdd726d34c46f25ffc85889fa3
BurmAlya <burmistrovaaleftina@yandex.ru>
2025-09-09 13:02:51 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+
+int main()
+{
+    list<int> listData;
+    
+    listData.push_back(7);
+    listData.push_back(17);
+    listData.push_back(27);
+    listData.push_back(37);
+
+    return 0;
